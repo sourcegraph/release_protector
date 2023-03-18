b9fde064f6e09e993ecb2b0f1e471cf59bf0923f
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2023-03-18 02:48:20 +0100

    add tests

---
+++ labels_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/google/go-github/v50/github"
+)
+
+func TestGennerateBackportLabelForRelease(t *testing.T) {
+	expected := "backport 2.0"
+	got := generateBackportLabelForRelease("2.0")
+
+	if got != expected {
+		t.Fatalf("expected %s got %s", expected, got)
+	}
+}
+
+func TestCheckForLabel(t *testing.T) {
+	labels := []*github.Label{
+		{Name: strToPtr("Label 1")},
+		{Name: strToPtr("Label 2")},
+		{Name: strToPtr("Label 3")},
+	}
+
+	t.Run("nonexistent label", func(t *testing.T) {
+		labelExists := checkForLabel(labels, "label 4")
+		if labelExists {
+			t.Fatal("expected label to not exist")
+		}
+	})
+
+	t.Run("existing label", func(t *testing.T) {
+		labelExists := checkForLabel(labels, "label 2")
+		if !labelExists {
+			t.Fatal("expected label to exist")
+		}
+	})
+}
+
+func strToPtr(input string) *string {
+	if input == "" {
+		return nil
+	}
+	return &input
+}
